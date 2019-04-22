3979837eec469d2aa20d864e299862991d407862
ImplicitDeny <quentin.aymard21@gmail.com>
2019-04-22 15:00:30 +0200

    Correcting displayed ballistic protection
    Sakuraba vests were displaying the wrong descriptionShort, misleading players into believing they were left unprotected against ballistic threats. This is now fixed. Only display has been changed, ballistic values themselves were already properly set.

---
+++ gsri_vests/config.cpp
@@ -0,0 +1,48 @@
+class CfgPatches {
+    class gsri_radio {
+        units[]={};
+        requiredVersion=0.1;
+        requiredAddons[]={"COS_gilet"};
+        author="[-GSRI-]Cheitan";
+    };
+};
+
+class CfgWeapons {
+    class Vest_NoCamo_Base;
+    class JPC_1_OD : Vest_NoCamo_Base {
+        descriptionShort="$STR_A3_SP_AL_IV";
+    };
+    class JPC_3_OD : Vest_NoCamo_Base {
+        descriptionShort="$STR_A3_SP_AL_IV";
+    };
+    class mbss_TL_od : Vest_NoCamo_Base {
+        descriptionShort="$STR_A3_SP_AL_IV";
+    };
+    class mbss_com_od : Vest_NoCamo_Base {
+        descriptionShort="$STR_A3_SP_AL_IV";
+    };
+    class mbss_SCOUT_od : Vest_NoCamo_Base {
+        descriptionShort="$STR_A3_SP_AL_IV";
+    };
+    class mbss_RECON_od : Vest_NoCamo_Base {
+        descriptionShort="$STR_A3_SP_AL_IV";
+    };
+    class 6094_com_OD : Vest_NoCamo_Base {
+        descriptionShort="$STR_A3_SP_AL_IV";
+    };
+    class 6094_SCOUT_od : Vest_NoCamo_Base {
+        descriptionShort="$STR_A3_SP_AL_IV";
+    };
+    class 6094_RECON_od : Vest_NoCamo_Base {
+        descriptionShort="$STR_A3_SP_AL_IV";
+    };
+    class 6094_MEDIC_od : Vest_NoCamo_Base {
+        descriptionShort="$STR_A3_SP_AL_IV";
+    };
+    class 6094_HEAVY_od : Vest_NoCamo_Base {
+        descriptionShort="$STR_A3_SP_AL_IV";
+    };
+    class 6094_JTAC : Vest_NoCamo_Base {
+        descriptionShort="$STR_A3_SP_AL_IV";
+    };
+};
