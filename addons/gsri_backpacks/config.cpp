3690216049eeb4cfdb72a2fb980f69c473635ee1
ImplicitDeny <quentin.aymard21@gmail.com>
2019-06-07 11:57:07 +0200

    Fixing bad indentation
    Old COS values were not properly indented, with invisible characters at line beginnings.

--- addons/gsri_backpacks/config.cpp
+++ addons/gsri_backpacks/config.cpp
@@ -84,22 +84,22 @@ class CfgVehicles {
         scope = 2;
         scopeArsenal = 2;
         author = "$STR_GSRI_backpack_author";
-		displayName="$STR_GSRI_backpack_A_displayName";
-		picture="COS_back_pack\data\icon\Sac_assausl_od.paa";
+	displayName="$STR_GSRI_backpack_A_displayName";
+	picture="COS_back_pack\data\icon\Sac_assausl_od.paa";
         model = "\COS_back_pack\KIT_BAG";
-		hiddenSelections[]={};
-		hiddenSelectionsTextures[]={};
-		hiddenSelectionsMaterials[]={};
+	hiddenSelections[]={};
+	hiddenSelectionsTextures[]={};
+	hiddenSelectionsMaterials[]={};
     };
     class GSRI_backpack_A_pince : GSRI_backpack_A {
-		displayName="$STR_GSRI_backpack_A_pince_displayName";
+	displayName="$STR_GSRI_backpack_A_pince_displayName";
         model = "\COS_back_pack\KIT_BAG_PINCE";
         mass = 75; //50 + 25 for the wirecutter
         //no change in default maxLoad (280) because the cutter hangs outside the bag
         ace_logistics_wirecutter_hasWireCutter = 1;
     };
     class GSRI_backpack_A_alpin : GSRI_backpack_A {
-		displayName="$STR_GSRI_backpack_A_alpin_displayName";
+	displayName="$STR_GSRI_backpack_A_alpin_displayName";
         model = "\COS_back_pack\KIT_BAG_Alpin";
     };
 
@@ -108,33 +108,33 @@ class CfgVehicles {
         scope = 2;
         scopeArsenal = 2;
         author = "$STR_GSRI_backpack_author";
-		displayName="$STR_GSRI_backpack_B_displayName";
-		picture="COS_back_pack\data\icon\sac_fs.paa";
-		model="\COS_back_pack\SOG_BAG";
-		hiddenSelections[]=
-		{
-			"camo",
-			"camo3"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"\COS_back_pack\data\SOG_BACKPACK\SOG_BAG_OD_co.paa",
-			"cos_back_pack\data\assets\assets_strap.co.paa"
-		};
+	displayName="$STR_GSRI_backpack_B_displayName";
+	picture="COS_back_pack\data\icon\sac_fs.paa";
+	model="\COS_back_pack\SOG_BAG";
+	hiddenSelections[]=
+	{
+		"camo",
+		"camo3"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"\COS_back_pack\data\SOG_BACKPACK\SOG_BAG_OD_co.paa",
+		"cos_back_pack\data\assets\assets_strap.co.paa"
+	};
     };
     class GSRI_backpack_B_pince : GSRI_backpack_B {
         displayName="$STR_GSRI_backpack_B_pince_displayName";
-		model="\COS_back_pack\SOG_BAG_BREACHER";
+	model="\COS_back_pack\SOG_BAG_BREACHER";
         ace_logistics_wirecutter_hasWireCutter = 1;
         mass = 45; //20+25 for the wirecutter
         //no change in default maxLoad (160) because the cutter hangs outside the bag
     };
     class GSRI_backpack_B_medic : GSRI_backpack_B {
         displayName="$STR_GSRI_backpack_B_medic_displayName";
-		model="\COS_back_pack\SOG_BAG_med";
+	model="\COS_back_pack\SOG_BAG_med";
     };
     class GSRI_backpack_B_alpin : GSRI_backpack_B {
         displayName="$STR_GSRI_backpack_B_alpin_displayName";
-		model="\COS_back_pack\SOG_BAG_ALPIN";
+	model="\COS_back_pack\SOG_BAG_ALPIN";
     };
 };
