52bf54803930d37a9537a8c215317653f0f14539
ImplicitDeny <quentin.aymard21@gmail.com>
2019-06-11 18:23:51 +0200

    More bad indent

--- addons/gsri_backpacks/config.cpp
+++ addons/gsri_backpacks/config.cpp
@@ -16,66 +16,59 @@ class CfgVehicles {
         scope = 1;
         scopeArsenal = 0;
         displayName="$STR_GSRI_backpack_obsolete_displayName";
-        class EventHandlers
-        {
-            init = "systemChat 'Using an old backpack classname, please consider switching to the new one !'";
+        class EventHandlers {
+            init="systemChat 'Using an old backpack classname, please consider switching to the new one !'";
         };
     };
     class KIT_BAG_od : B_AssaultPack_mcamo {
         scope = 1;
         scopeArsenal = 0;
         displayName="$STR_GSRI_backpack_obsolete_displayName";
-        class EventHandlers
-        {
-            init = "systemChat 'Using an old backpack classname, please consider switching to the new one !'";
+        class EventHandlers {
+            init="systemChat 'Using an old backpack classname, please consider switching to the new one !'";
         };
     };
     class KIT_BAG_pince_OD : B_AssaultPack_mcamo {
         scope = 1;
         scopeArsenal = 0;
         displayName="$STR_GSRI_backpack_obsolete_displayName";
-        class EventHandlers
-        {
-            init = "systemChat 'Using an old backpack classname, please consider switching to the new one !'";
+        class EventHandlers {
+            init="systemChat 'Using an old backpack classname, please consider switching to the new one !'";
         };
     };
     class SOG_BAG_ALPIN_od : B_AssaultPack_mcamo {
         scope = 1;
         scopeArsenal = 0;
         displayName="$STR_GSRI_backpack_obsolete_displayName";
-        class EventHandlers
-        {
-            init = "systemChat 'Using an old backpack classname, please consider switching to the new one !'";
+        class EventHandlers {
+            init="systemChat 'Using an old backpack classname, please consider switching to the new one !'";
         };
     };
     class SOG_BAG_BREACHER_od : B_AssaultPack_mcamo {
         scope = 1;
         scopeArsenal = 0;
         displayName="$STR_GSRI_backpack_obsolete_displayName";
-        class EventHandlers
-        {
-            init = "systemChat 'Using an old backpack classname, please consider switching to the new one !'";
+        class EventHandlers {
+            init="systemChat 'Using an old backpack classname, please consider switching to the new one !'";
         };
     };
     class SOG_BAG_med_od : B_AssaultPack_mcamo {
         scope = 1;
         scopeArsenal = 0;
         displayName="$STR_GSRI_backpack_obsolete_displayName";
-        class EventHandlers
-        {
-            init = "systemChat 'Using an old backpack classname, please consider switching to the new one !'";
+        class EventHandlers {
+            init="systemChat 'Using an old backpack classname, please consider switching to the new one !'";
         };
     };
     class SOG_BAG_OD : B_AssaultPack_mcamo {
         scope = 1;
         scopeArsenal = 0;
         displayName="$STR_GSRI_backpack_obsolete_displayName";
-        class EventHandlers
-        {
-            init = "systemChat 'Using an old backpack classname, please consider switching to the new one !'";
+        class EventHandlers {
+            init="systemChat 'Using an old backpack classname, please consider switching to the new one !'";
         };
     };
-    
+
 
     //Create new items with fixed values
     //for the radio variant, see gsri_radio.pbo
@@ -84,22 +77,22 @@ class CfgVehicles {
         scope = 2;
         scopeArsenal = 2;
         author = "$STR_GSRI_backpack_author";
-	displayName="$STR_GSRI_backpack_A_displayName";
-	picture="COS_back_pack\data\icon\Sac_assausl_od.paa";
+        displayName="$STR_GSRI_backpack_A_displayName";
+        picture="COS_back_pack\data\icon\Sac_assausl_od.paa";
         model = "\COS_back_pack\KIT_BAG";
-	hiddenSelections[]={};
-	hiddenSelectionsTextures[]={};
-	hiddenSelectionsMaterials[]={};
+        hiddenSelections[]={};
+        hiddenSelectionsTextures[]={};
+        hiddenSelectionsMaterials[]={};
     };
     class GSRI_backpack_A_pince : GSRI_backpack_A {
-	displayName="$STR_GSRI_backpack_A_pince_displayName";
+        displayName="$STR_GSRI_backpack_A_pince_displayName";
         model = "\COS_back_pack\KIT_BAG_PINCE";
         mass = 75; //50 + 25 for the wirecutter
         //no change in default maxLoad (280) because the cutter hangs outside the bag
         ace_logistics_wirecutter_hasWireCutter = 1;
     };
-    class GSRI_backpack_A_alpin : GSRI_backpack_A {
-	displayName="$STR_GSRI_backpack_A_alpin_displayName";
+        class GSRI_backpack_A_alpin : GSRI_backpack_A {
+        displayName="$STR_GSRI_backpack_A_alpin_displayName";
         model = "\COS_back_pack\KIT_BAG_Alpin";
     };
 
@@ -108,33 +101,28 @@ class CfgVehicles {
         scope = 2;
         scopeArsenal = 2;
         author = "$STR_GSRI_backpack_author";
-	displayName="$STR_GSRI_backpack_B_displayName";
-	picture="COS_back_pack\data\icon\sac_fs.paa";
-	model="\COS_back_pack\SOG_BAG";
-	hiddenSelections[]=
-	{
-		"camo",
-		"camo3"
-	};
-	hiddenSelectionsTextures[]=
-	{
-		"\COS_back_pack\data\SOG_BACKPACK\SOG_BAG_OD_co.paa",
-		"cos_back_pack\data\assets\assets_strap.co.paa"
-	};
+        displayName="$STR_GSRI_backpack_B_displayName";
+        picture="COS_back_pack\data\icon\sac_fs.paa";
+        model="\COS_back_pack\SOG_BAG";
+        hiddenSelections[]={"camo","camo3"};
+        hiddenSelectionsTextures[]={
+            "\COS_back_pack\data\SOG_BACKPACK\SOG_BAG_OD_co.paa",
+            "cos_back_pack\data\assets\assets_strap.co.paa"
+        };
     };
     class GSRI_backpack_B_pince : GSRI_backpack_B {
         displayName="$STR_GSRI_backpack_B_pince_displayName";
-	model="\COS_back_pack\SOG_BAG_BREACHER";
+        model="\COS_back_pack\SOG_BAG_BREACHER";
         ace_logistics_wirecutter_hasWireCutter = 1;
         mass = 45; //20+25 for the wirecutter
         //no change in default maxLoad (160) because the cutter hangs outside the bag
     };
     class GSRI_backpack_B_medic : GSRI_backpack_B {
         displayName="$STR_GSRI_backpack_B_medic_displayName";
-	model="\COS_back_pack\SOG_BAG_med";
+        model="\COS_back_pack\SOG_BAG_med";
     };
     class GSRI_backpack_B_alpin : GSRI_backpack_B {
         displayName="$STR_GSRI_backpack_B_alpin_displayName";
-	model="\COS_back_pack\SOG_BAG_ALPIN";
+        model="\COS_back_pack\SOG_BAG_ALPIN";
     };
 };
