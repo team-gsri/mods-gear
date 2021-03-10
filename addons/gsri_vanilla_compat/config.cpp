5cd0d4a8ee4238a05e8dff5fe1bed21151e65500
ImplicitDeny <quentin.aymard21@gmail.com>
2021-03-10 21:52:47 +0100

    Add SMA mags to CBA Stanag magwell
    Custom magwell array for better consistency. All 9 SMA 556 magazines added.

---
+++ addons/gsri_vanilla_compat/config.cpp
@@ -0,0 +1,24 @@
+class CfgPatches {
+    class gsri_vanilla_compat {
+        units[]={};
+        requiredVersion=0.1;
+        requiredAddons[]={"cba_jr", "cba_jam"};
+        author="[-GSRI-] Cheitan";
+    };
+};
+
+class CfgMagazineWells {
+    class CBA_556x45_STANAG {
+        SMA_mags[] = {
+            "SMA_30Rnd_556x45_M855A1",
+            "SMA_30Rnd_556x45_M855A1_Tracer",
+            "SMA_30Rnd_556x45_M855A1_IR",
+            "SMA_30Rnd_556x45_Mk318",
+            "SMA_30Rnd_556x45_Mk318_Tracer",
+            "SMA_30Rnd_556x45_Mk318_IR",
+            "SMA_30Rnd_556x45_Mk262",
+            "SMA_30Rnd_556x45_Mk262_Tracer",
+            "SMA_30Rnd_556x45_Mk262_IR"
+        };
+    };
+};
