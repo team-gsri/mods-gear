f10d10d957e6e9eb77fa86286589bf0ce4a9707e
ImplicitDeny <quentin.aymard21@gmail.com>
2021-03-10 23:23:12 +0100

    Add MSBS compat with modded optics
    ASDG/CBA compat is working on all MSBS serie. Muzzle will not be modified. Side rail is still to be explored. Also gripod feature might be possible for compact versions.

--- addons/gsri_vanilla_compat/config.cpp
+++ addons/gsri_vanilla_compat/config.cpp
@@ -23,9 +23,20 @@ class CfgMagazineWells {
     };
 };
 
+class asdg_OpticRail1913;
+
 class CfgWeapons {
-    class Rifle_Base_F;
+    class Rifle;
+    class Rifle_Base_F : Rifle {
+        class WeaponSlotsInfo;
+    };
     class arifle_MSBS65_base_F : Rifle_Base_F {
         magazineWell[] += {"CBA_556x45_STANAG"};
+        class WeaponSlotsInfo: WeaponSlotsInfo {
+            class CowsSlot: asdg_OpticRail1913 {
+                iconPosition[] = {0.45,0.28};
+                iconScale = 0.2;
+            };
+        };
     };
 };
