d170174ecbf3a4ae049aaf3b6e164ef3c5f97c7b
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2021-06-19 19:33:14 +0200

    Add european flag

--- addons/gsri_flags/config.cpp
+++ addons/gsri_flags/config.cpp
@@ -13,7 +13,7 @@ class CfgPatches
 class CfgVehicles
 {
 	class FlagCarrier;
-	class gsri_flags_gsri : FlagCarrier
+	class gsri_flags_base : FlagCarrier
 	{
 		author="www.gsri.team";
 		class SimpleObject
@@ -31,10 +31,8 @@ class CfgVehicles
 			verticalOffsetWorld=0;
 			init="''";
 		};
-		editorPreview="\fr\gsri\flags\flag_gsri.jpg";
 		scope=2;
 		scopeCurator=2;
-		displayName="$STR_GSRI_FLAG_GSRI";
 		hiddenSelectionsTextures[]=
 		{
 			"\A3\Structures_F\Mil\Flags\Data\Mast_mil_CO.paa"
@@ -43,44 +41,32 @@ class CfgVehicles
 		{
 			"\A3\Structures_F\Mil\Flags\Data\Mast_mil.rvmat"
 		};
+	};
+	class gsri_flags_gsri : gsri_flags_base
+	{
+		editorPreview="\fr\gsri\flags\flag_gsri.jpg";
+		displayName="$STR_GSRI_FLAG_GSRI";
 		class EventHandlers
 		{
 			init="(_this select 0) setFlagTexture '\fr\gsri\flags\flag_gsri_CO.paa'";
 		};
 	};
-	class gsri_flags_fr : FlagCarrier
+	class gsri_flags_fr : gsri_flags_base
 	{
-		author="www.gsri.team";
-		class SimpleObject
-		{
-			eden=0;
-			animate[]=
-			{				
-				{
-					"flag",
-					0
-				}
-			};
-			hide[]={};
-			verticalOffset=3.977;
-			verticalOffsetWorld=0;
-			init="''";
-		};
 		editorPreview="\fr\gsri\flags\flag_fr.jpg";
-		scope=2;
-		scopeCurator=2;
 		displayName="$STR_GSRI_FLAG_FR";
-		hiddenSelectionsTextures[]=
-		{
-			"\A3\Structures_F\Mil\Flags\Data\Mast_mil_CO.paa"
-		};
-		hiddenSelectionsMaterials[]=
-		{
-			"\A3\Structures_F\Mil\Flags\Data\Mast_mil.rvmat"
-		};
 		class EventHandlers
 		{
 			init="(_this select 0) setFlagTexture '\fr\gsri\flags\flag_fr_CO.paa'";
+		};
+	};
+	class gsri_flags_eu : gsri_flags_base
+	{
+		editorPreview="\fr\gsri\flags\flag_eu.jpg";
+		displayName="$STR_GSRI_FLAG_EU";
+		class EventHandlers
+		{
+			init="(_this select 0) setFlagTexture '\fr\gsri\flags\flag_eu_CO.paa'";
 		};
 	};
 };
