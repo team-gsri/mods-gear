6571397dbfba77cd27214bea7c5987e88938ac45
ImplicitDeny <quentin.aymard21@gmail.com>
2022-03-05 19:13:38 +0100

    Add prefix and cleanup

--- addons/gsri_uniforms/config.cpp
+++ addons/gsri_uniforms/config.cpp
@@ -1,177 +1,74 @@
-class CfgPatches
-{
-	class GSRI_UNIFORMS_Config
-	{
+class CfgPatches {
+	class gsri_uniforms	{
 		author="[-GSRI-] PhileasLX";
 		units[]={};
 		weapons[]={};
 		requiredVersion=0.1;
-		requiredAddons[]=
-		{
+		requiredAddons[] = {
 			"A3_Characters_F_EXP", 
 			"A3_Characters_F"
 		};
 	};
 };
-class CfgVehicleClasses
-{
-	class GSRI_UNIFORMS
-	{
-		displayName="GSRI Uniforms";
-	};
-};
-class cfgVehicles
-{
+
+class CfgVehicles {
 	class B_Soldier_F;
-
-	////////////////////
-	////  UNIFORMS  ////
-	////////////////////
-
-	class GSRI_uniform_U: B_Soldier_F
-	{
+	class GSRI_uniform_U: B_Soldier_F {
 		faction="BLU_F";
 		modelsides[]={3,2,1,0};
 		displayName="[-GSRI-] Uniform";
 		model="\A3\Characters_F\BLUFOR\B_Soldier_01.p3d";
 		uniformClass="GSRI_uniform";
 		hiddenSelections[] = {"camo","insignia"};
-		hiddenSelectionsTextures[] = {"gsri_uniforms\Data\gsri_uniform_co.paa"};
-		hiddenSelectionsMaterials[] = {"gsri_uniforms\Data\gsri_uniform.rvmat"};
-		linkedItems[]=
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
-		};
-		respawnLinkedItems[]=
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
-		};
+		hiddenSelectionsTextures[] = {"fr\gsri\uniforms\Data\gsri_uniform_co.paa"};
+		hiddenSelectionsMaterials[] = {"fr\gsri\uniforms\Data\gsri_uniform.rvmat"};
+		linkedItems[] = {};
+		respawnLinkedItems[] = {};
+		weapons[] = {};
+		respawnWeapons[] = {};
+		magazines[] = {};
+		respawnMagazines[] = {};
+		items[] = {};
+		respawnItems[] = {};
 	};
-	class GSRI_uniform_raised_collar_U: B_Soldier_F
-	{
-		faction="BLU_F";
+	class GSRI_uniform_raised_collar_U: GSRI_uniform_U {
 		modelsides[]={3,2,1,0};
 		displayName="[-GSRI-] Uniform (Raised Collar)";
 		model="\A3\Characters_F_Exp\BLUFOR\B_CTRG_Soldier_01_F.p3d";
 		uniformClass="GSRI_uniform_raised_collar";
-		hiddenSelections[] = {"camo","insignia"};
-		hiddenSelectionsTextures[] = {"gsri_uniforms\Data\gsri_uniform_co.paa"};
-		hiddenSelectionsMaterials[] = {"gsri_uniforms\Data\gsri_uniform.rvmat"};
-		linkedItems[]=
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
-		};
-		respawnLinkedItems[]=
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
-		};
 	};
-	class GSRI_uniform_rolled_U: B_Soldier_F
-	{
-		faction="BLU_F";
-		modelsides[]={3,2,1,0};
+	class GSRI_uniform_rolled_U: GSRI_uniform_U {
 		displayName="[-GSRI-] Uniform (Rolled)";
 		model="\A3\Characters_F\BLUFOR\B_Soldier_03.p3d";
 		uniformClass="GSRI_uniform_rolled";
-		hiddenSelections[] = {"camo","insignia"};
-		hiddenSelectionsTextures[] = {"gsri_uniforms\Data\gsri_uniform_co.paa"};
-		hiddenSelectionsMaterials[] = {"gsri_uniforms\Data\gsri_uniform.rvmat"};
-		linkedItems[]=
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
-		};
-		respawnLinkedItems[]=
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
-		};
 	};
 
-	////////////////////
-	////   SHIRTS   ////
-	////////////////////
-
-	class GSRI_shirt_green_U: B_Soldier_F
-	{
-		faction="BLU_F";
-		modelsides[]={3,2,1,0};
+	class GSRI_shirt_green_U: GSRI_uniform_U {
 		displayName="[-GSRI-] Shirt Green/Camo";
 		model="\A3\Characters_F\BLUFOR\B_Soldier_02.p3d";
 		uniformClass="GSRI_shirt_green";
 		hiddenSelections[] = {"camo","camo2","insignia"};
-		hiddenSelectionsTextures[] = 
-			{
-				"gsri_uniforms\Data\gsri_uniform_co.paa",
-				"gsri_uniforms\Data\gsri_shirt_green_co.paa"
-			};
-		hiddenSelectionsMaterials[] = 
-			{
-				"gsri_uniforms\Data\gsri_uniform.rvmat",
-				"a3\characters_f\common\data\basicbody.rvmat"
-			};
-		linkedItems[]=
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
+		hiddenSelectionsTextures[] = {
+			"fr\gsri\uniforms\Data\gsri_uniform_co.paa",
+			"fr\gsri\uniforms\Data\gsri_shirt_green_co.paa"
 		};
-		respawnLinkedItems[]=
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
+		hiddenSelectionsMaterials[] = {
+			"fr\gsri\uniforms\Data\gsri_uniform.rvmat",
+			"a3\characters_f\common\data\basicbody.rvmat"
 		};
 	};
-	class GSRI_shirt_camo_U: B_Soldier_F
-	{
-		faction="BLU_F";
-		modelsides[]={3,2,1,0};
+	class GSRI_shirt_camo_U: GSRI_uniform_U {
 		displayName="[-GSRI-] Shirt Camo/Green";
 		model="\A3\Characters_F\BLUFOR\B_Soldier_02.p3d";
 		uniformClass="GSRI_shirt_camo";
 		hiddenSelections[] = {"camo","camo2","insignia"};
-		hiddenSelectionsTextures[] = 
-			{
-				"gsri_uniforms\Data\gsri_uniform_co.paa",
-				"gsri_uniforms\Data\gsri_shirt_camo_co.paa"
-			};
-		hiddenSelectionsMaterials[] = 
-			{
-				"gsri_uniforms\Data\gsri_uniform.rvmat",
-				"a3\characters_f\common\data\basicbody.rvmat"
-			};
-		linkedItems[]=
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
+		hiddenSelectionsTextures[] = {
+			"fr\gsri\uniforms\Data\gsri_uniform_co.paa",
+			"fr\gsri\uniforms\Data\gsri_shirt_camo_co.paa"
 		};
-		respawnLinkedItems[]=
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
+		hiddenSelectionsMaterials[] = {
+			"fr\gsri\uniforms\Data\gsri_uniform.rvmat",
+			"a3\characters_f\common\data\basicbody.rvmat"
 		};
 	};
 };	
@@ -179,104 +76,64 @@ class cfgWeapons
 {
 	class ItemCore;
 	class UniformItem;
-	class Uniform_Base: ItemCore
-	{
+	class Uniform_Base: ItemCore {
 		class ItemInfo;
 	};
 
-	////////////////////
-	////  UNIFORMS  ////
-	////////////////////
-	
-	class GSRI_uniform: Uniform_Base
-	{
+	class GSRI_uniform: Uniform_Base {
 		author="GSRI";
 		scope=2;
 		displayName="[-GSRI-] Uniform";
-		picture="\gsri_uniforms\UI\icon_gsri_uniform.paa";
+		picture="fr\gsri\uniforms\UI\icon_gsri_uniform.paa";
 		modelsides[]={3,2,1,0};
 		model="\A3\Characters_F\BLUFOR\B_Soldier_01.p3d";
 		hiddenSelections[] = {"camo"};
 		hiddenSelectionsTextures[] = {};
-		class ItemInfo: UniformItem
-		{
+		class ItemInfo: UniformItem {
 			uniformModel="-";
 			uniformClass="GSRI_uniform_U";
 			containerClass="Supply50";
 			mass=25;
 		};
 	};
-	class GSRI_uniform_raised_collar: Uniform_Base
-	{
-		author="GSRI";
-		scope=2;
+	class GSRI_uniform_raised_collar: GSRI_uniform {
 		displayName="[-GSRI-] Uniform (Raised Collar)";
-		picture="\gsri_uniforms\UI\icon_gsri_uniform.paa";
-		modelsides[]={3,2,1,0};
+		picture="fr\gsri\uniforms\UI\icon_gsri_uniform.paa";
 		model="\A3\Characters_F_Exp\BLUFOR\B_CTRG_Soldier_01_F.p3d";
-		hiddenSelections[] = {"camo"};
-		hiddenSelectionsTextures[] = {};
-		class ItemInfo: UniformItem
-		{
+		class ItemInfo: UniformItem {
 			uniformClass="GSRI_uniform_raised_collar_U";
 			containerClass="Supply50";
 			mass=25;
 		};
 	};
-	class GSRI_uniform_rolled: Uniform_Base
-	{
-		author="GSRI";
-		scope=2;
+	class GSRI_uniform_rolled: GSRI_uniform {
 		displayName="[-GSRI-] Uniform (Rolled)";
-		picture="\gsri_uniforms\UI\icon_gsri_uniform.paa";
-		modelsides[]={3,2,1,0};
+		picture="fr\gsri\uniforms\UI\icon_gsri_uniform.paa";
 		model="\A3\Characters_F\BLUFOR\B_Soldier_03.p3d";
-		hiddenSelections[] = {"camo"};
-		hiddenSelectionsTextures[] = {};
-		class ItemInfo: UniformItem
-		{
-			uniformModel="-";
+		class ItemInfo: UniformItem {
 			uniformClass="GSRI_uniform_rolled_U";
 			containerClass="Supply50";
 			mass=25;
 		};
 	};
 
-	////////////////////
-	////   SHIRTS   ////
-	////////////////////
-
-	class GSRI_shirt_green: Uniform_Base
-	{
-		author="GSRI";
-		scope=2;
+	class GSRI_shirt_green: GSRI_uniform {
 		displayName="[-GSRI-] Shirt Green/Camo";
-		picture="\gsri_uniforms\UI\icon_gsri_shirt.paa";
-		modelsides[]={3,2,1,0};
+		picture="fr\gsri\uniforms\UI\icon_gsri_shirt.paa";
 		model="\A3\Characters_F\BLUFOR\B_Soldier_02.p3d";
 		hiddenSelections[] = {"camo","camo2"};
-		hiddenSelectionsTextures[] = {};
-		class ItemInfo: UniformItem
-		{
-			uniformModel="-";
+		class ItemInfo: UniformItem {
 			uniformClass="GSRI_shirt_green_U";
 			containerClass="Supply50";
 			mass=25;
 		};
 	};
-	class GSRI_shirt_camo: Uniform_Base
-	{
-		author="GSRI";
-		scope=2;
+	class GSRI_shirt_camo: GSRI_uniform {
 		displayName="[-GSRI-] Shirt Camo/Green";
-		picture="\gsri_uniforms\UI\icon_gsri_shirt.paa";
-		modelsides[]={3,2,1,0};
+		picture="fr\gsri\uniforms\UI\icon_gsri_shirt.paa";
 		model="\A3\Characters_F\BLUFOR\B_Soldier_02.p3d";
 		hiddenSelections[] = {"camo","camo2"};
-		hiddenSelectionsTextures[] = {};
-		class ItemInfo: UniformItem
-		{
-			uniformModel="-";
+		class ItemInfo: UniformItem {
 			uniformClass="GSRI_shirt_camo_U";
 			containerClass="Supply50";
 			mass=25;
