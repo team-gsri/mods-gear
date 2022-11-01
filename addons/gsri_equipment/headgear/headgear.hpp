fac235e86a124363f62307258b1d16d7ba3b91d0
ImplicitDeny <17797285+ImplicitDeny@users.noreply.github.com>
2022-11-01 22:02:45 +0100

    Fix: hearing protection (#36)
    * Fix: wrong ballistic description : unify description for all helmets, ballistic protection was already ok
    
    * Fix: ACE hearing values (Peltor standards) : also add missing helmet variant.

--- addons/gsri_equipment/headgear/headgear.hpp
+++ addons/gsri_equipment/headgear/headgear.hpp
@@ -27,6 +27,7 @@ class GSRI_helmet : H_HelmetB {
     author = "$STR_GSRI_AUTHORS_PHILEAS";
     weaponPoolAvailable = 1;
     displayName = "$STR_GSRI_HEADGEAR_HELMET";
+    descriptionShort="$STR_A3_SP_AL_II";
     picture = "\fr\gsri\equipment\headgear\UI\icon_gsri_helmet.paa";
     model = "\A3\Characters_F\BLUFOR\headgear_b_helmet_plain.p3d";
     hiddenSelections[] = { "camo" };
@@ -51,6 +52,7 @@ class GSRI_helmet_spec : H_HelmetSpecB {
     author = "$STR_GSRI_AUTHORS_PHILEAS";
     weaponPoolAvailable = 1;
     displayName = "$STR_GSRI_HEADGEAR_HELMET_ACCESSORIES";
+    descriptionShort="$STR_A3_SP_AL_II";
     picture = "\fr\gsri\equipment\headgear\UI\icon_gsri_helmet_accessories.paa";
     model = "\A3\Characters_F\BLUFOR\headgear_b_helmet_ballistic.p3d";
     hiddenSelections[] = { "camo" };
@@ -75,6 +77,7 @@ class GSRI_helmet_light : H_HelmetB_light {
     author = "$STR_GSRI_AUTHORS_PHILEAS";
     weaponPoolAvailable = 1;
     displayName = "$STR_GSRI_HEADGEAR_HELMET_LIGHT";
+    descriptionShort="$STR_A3_SP_AL_II";
     picture = "\fr\gsri\equipment\headgear\UI\icon_gsri_helmet_light.paa";
     model = "\A3\Characters_F\BLUFOR\headgear_b_helmet_light.p3d";
     hiddenSelections[] = { "camo" };
@@ -99,6 +102,7 @@ class GSRI_helmet_modular : H_HelmetB {
     author = "$STR_GSRI_AUTHORS_PHILEAS";
     weaponPoolAvailable = 1;
     displayName = "$STR_GSRI_HEADGEAR_HELMET_MODULAR";
+    descriptionShort="$STR_A3_SP_AL_II";
     picture = "\fr\gsri\equipment\headgear\UI\icon_gsri_helmet_modular.paa";
     model = "\A3\Characters_F_Enoch\Headgear\H_HelmetHBK_01_F.p3d";
     hiddenSelections[] = { "camo" };
@@ -118,11 +122,14 @@ class GSRI_helmet_modular : H_HelmetB {
     };
 };
 
-class GSRI_helmet_modular_advanced : GSRI_helmet_modular {
+class GSRI_helmet_modular_advanced : H_HelmetSpecB {
     author = "$STR_GSRI_AUTHORS_PHILEAS";
     displayName = "$STR_GSRI_HEADGEAR_HELMET_MODULAR_ACCESSORIES";
+    descriptionShort="$STR_A3_SP_AL_II";
     picture = "\fr\gsri\equipment\headgear\UI\icon_gsri_helmet_modular_accessories.paa";
     model = "\A3\Characters_F_Enoch\Headgear\H_HelmetHBK_01_headset_F.p3d";
+    hiddenSelections[] = { "camo" };
+    hiddenSelectionsTextures[] = { "\fr\gsri\equipment\headgear\Data\helmet_modular_co.paa"};
     class ItemInfo : HeadgearItem {
         mass = 30;
         uniformModel = "\A3\Characters_F_Enoch\Headgear\H_HelmetHBK_01_headset_F.p3d";
@@ -138,9 +145,31 @@ class GSRI_helmet_modular_advanced : GSRI_helmet_modular {
     };
 };
 
-class GSRI_helmet_modular_integral : GSRI_helmet_modular {
+class GSRI_helmet_modular_halfintegral : GSRI_helmet_modular_advanced {
+    author = "$STR_GSRI_AUTHORS_PHILEAS";
+    displayName = "$STR_GSRI_HEADGEAR_HELMET_MODULAR_HALFINTEGRAL";
+    descriptionShort="$STR_A3_SP_AL_II";
+    picture = "\fr\gsri\equipment\headgear\UI\icon_gsri_helmet_modular_accessories.paa";
+    model = "\A3\Characters_F_Enoch\Headgear\H_HelmetHBK_01_ear_F.p3d";
+    class ItemInfo : HeadgearItem {
+        mass = 30;
+        uniformModel = "\A3\Characters_F_Enoch\Headgear\H_HelmetHBK_01_ear_F.p3d";
+        modelSides[] = { TCivilian, TWest };
+        hiddenSelections[] = { "camo" };
+        class HitpointsProtectionInfo {
+            class Head {
+                hitPointName = "HitHead";
+                armor = 6;
+                passThrough = 0.5;
+            };
+        };
+    };
+};
+
+class GSRI_helmet_modular_integral : GSRI_helmet_modular_advanced {
     author = "$STR_GSRI_AUTHORS_PHILEAS";
     displayName = "$STR_GSRI_HEADGEAR_HELMET_MODULAR_INTEGRAL";
+    descriptionShort="$STR_A3_SP_AL_II";
     picture = "\fr\gsri\equipment\headgear\UI\icon_gsri_helmet_modular_accessories.paa";
     model = "\A3\Characters_F_Enoch\Headgear\H_HelmetHBK_01_chops_F.p3d";
     class ItemInfo : HeadgearItem {
