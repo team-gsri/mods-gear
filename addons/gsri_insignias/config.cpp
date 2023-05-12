647b8a4bbd4170e33e7db4fe7b0235489c986484
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2023-05-12 14:48:24 +0200

    feat: added gcap insignias

---
+++ addons/gsri_insignias/config.cpp
@@ -0,0 +1,99 @@
+class CfgPatches {
+  class gsri_props {
+    author="www.gsri.team";
+    requiredVersion=0.1;
+    requiredAddons[]={};
+    units[]={};
+  };
+};
+
+class CfgUnitInsignia
+{
+	class GCAP_Blue_0 {
+		displayName = "$STR_GSRI_Insignias_Blue_0";
+		author = "www.gsri.team";
+		texture = "fr\gsri\insignias\textures\gcap_blue_0.paa";
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";
+	};
+	class GCAP_Blue_1 {
+		displayName = "$STR_GSRI_Insignias_Blue_1";
+		author = "www.gsri.team";
+		texture = "fr\gsri\insignias\textures\gcap_blue_1.paa";
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";
+	};
+	class GCAP_Blue_2 {
+		displayName = "$STR_GSRI_Insignias_Blue_2";
+		author = "www.gsri.team";
+		texture = "fr\gsri\insignias\textures\gcap_blue_2.paa";
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";
+	};
+  
+	class GCAP_Red_0 {
+		displayName = "$STR_GSRI_Insignias_Red_0";
+		author = "www.gsri.team";
+		texture = "fr\gsri\insignias\textures\gcap_red_0.paa";
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";
+	};
+	class GCAP_Red_1 {
+		displayName = "$STR_GSRI_Insignias_Red_1";
+		author = "www.gsri.team";
+		texture = "fr\gsri\insignias\textures\gcap_red_1.paa";
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";
+	};
+	class GCAP_Red_2 {
+		displayName = "$STR_GSRI_Insignias_Red_2";
+		author = "www.gsri.team";
+		texture = "fr\gsri\insignias\textures\gcap_red_2.paa";
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";
+	};
+  
+	class GCAP_Yellow_0 {
+		displayName = "$STR_GSRI_Insignias_Yellow_0";
+		author = "www.gsri.team";
+		texture = "fr\gsri\insignias\textures\gcap_yellow_0.paa";
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";
+	};
+	class GCAP_Yellow_1 {
+		displayName = "$STR_GSRI_Insignias_Yellow_1";
+		author = "www.gsri.team";
+		texture = "fr\gsri\insignias\textures\gcap_yellow_1.paa";
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";
+	};
+	class GCAP_Yellow_2 {
+		displayName = "$STR_GSRI_Insignias_Yellow_2";
+		author = "www.gsri.team";
+		texture = "fr\gsri\insignias\textures\gcap_yellow_2.paa";
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";
+	};
+  
+	class GCAP_Green_0 {
+		displayName = "$STR_GSRI_Insignias_Green_0";
+		author = "www.gsri.team";
+		texture = "fr\gsri\insignias\textures\gcap_green_0.paa";
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";
+	};
+	class GCAP_Green_1 {
+		displayName = "$STR_GSRI_Insignias_Green_1";
+		author = "www.gsri.team";
+		texture = "fr\gsri\insignias\textures\gcap_green_1.paa";
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";
+	};
+	class GCAP_Green_2 {
+		displayName = "$STR_GSRI_Insignias_Green_2";
+		author = "www.gsri.team";
+		texture = "fr\gsri\insignias\textures\gcap_green_2.paa";
+		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+		textureVehicle = "";
+	};
+};
