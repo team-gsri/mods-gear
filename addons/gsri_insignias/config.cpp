a5fd08fc7c87f7efd074407e0dfe1093757c83be
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2023-05-13 16:01:54 +0200

    fix: use stringtable for authors

--- addons/gsri_insignias/config.cpp
+++ addons/gsri_insignias/config.cpp
@@ -1,6 +1,6 @@
 class CfgPatches {
   class gsri_props {
-    author="www.gsri.team";
+    author="$STR_GSRI_AUTHORS";
     requiredVersion=0.1;
     requiredAddons[]={};
     units[]={};
@@ -11,21 +11,21 @@ class CfgUnitInsignia
 {
 	class GCAP_Blue_0 {
 		displayName = "$STR_GSRI_Insignias_Blue_0";
-		author = "www.gsri.team";
+		author = "$STR_GSRI_AUTHORS";
 		texture = "fr\gsri\insignias\textures\gcap_blue_0.paa";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 		textureVehicle = "";
 	};
 	class GCAP_Blue_1 {
 		displayName = "$STR_GSRI_Insignias_Blue_1";
-		author = "www.gsri.team";
+		author = "$STR_GSRI_AUTHORS";
 		texture = "fr\gsri\insignias\textures\gcap_blue_1.paa";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 		textureVehicle = "";
 	};
 	class GCAP_Blue_2 {
 		displayName = "$STR_GSRI_Insignias_Blue_2";
-		author = "www.gsri.team";
+		author = "$STR_GSRI_AUTHORS";
 		texture = "fr\gsri\insignias\textures\gcap_blue_2.paa";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 		textureVehicle = "";
@@ -33,21 +33,21 @@ class CfgUnitInsignia
   
 	class GCAP_Red_0 {
 		displayName = "$STR_GSRI_Insignias_Red_0";
-		author = "www.gsri.team";
+		author = "$STR_GSRI_AUTHORS";
 		texture = "fr\gsri\insignias\textures\gcap_red_0.paa";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 		textureVehicle = "";
 	};
 	class GCAP_Red_1 {
 		displayName = "$STR_GSRI_Insignias_Red_1";
-		author = "www.gsri.team";
+		author = "$STR_GSRI_AUTHORS";
 		texture = "fr\gsri\insignias\textures\gcap_red_1.paa";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 		textureVehicle = "";
 	};
 	class GCAP_Red_2 {
 		displayName = "$STR_GSRI_Insignias_Red_2";
-		author = "www.gsri.team";
+		author = "$STR_GSRI_AUTHORS";
 		texture = "fr\gsri\insignias\textures\gcap_red_2.paa";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 		textureVehicle = "";
@@ -55,21 +55,21 @@ class CfgUnitInsignia
   
 	class GCAP_Yellow_0 {
 		displayName = "$STR_GSRI_Insignias_Yellow_0";
-		author = "www.gsri.team";
+		author = "$STR_GSRI_AUTHORS";
 		texture = "fr\gsri\insignias\textures\gcap_yellow_0.paa";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 		textureVehicle = "";
 	};
 	class GCAP_Yellow_1 {
 		displayName = "$STR_GSRI_Insignias_Yellow_1";
-		author = "www.gsri.team";
+		author = "$STR_GSRI_AUTHORS";
 		texture = "fr\gsri\insignias\textures\gcap_yellow_1.paa";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 		textureVehicle = "";
 	};
 	class GCAP_Yellow_2 {
 		displayName = "$STR_GSRI_Insignias_Yellow_2";
-		author = "www.gsri.team";
+		author = "$STR_GSRI_AUTHORS";
 		texture = "fr\gsri\insignias\textures\gcap_yellow_2.paa";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 		textureVehicle = "";
@@ -77,21 +77,21 @@ class CfgUnitInsignia
   
 	class GCAP_Green_0 {
 		displayName = "$STR_GSRI_Insignias_Green_0";
-		author = "www.gsri.team";
+		author = "$STR_GSRI_AUTHORS";
 		texture = "fr\gsri\insignias\textures\gcap_green_0.paa";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 		textureVehicle = "";
 	};
 	class GCAP_Green_1 {
 		displayName = "$STR_GSRI_Insignias_Green_1";
-		author = "www.gsri.team";
+		author = "$STR_GSRI_AUTHORS";
 		texture = "fr\gsri\insignias\textures\gcap_green_1.paa";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 		textureVehicle = "";
 	};
 	class GCAP_Green_2 {
 		displayName = "$STR_GSRI_Insignias_Green_2";
-		author = "www.gsri.team";
+		author = "$STR_GSRI_AUTHORS";
 		texture = "fr\gsri\insignias\textures\gcap_green_2.paa";
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
 		textureVehicle = "";
